f9025e64aaa37b5501968b1fb65529f67d1f51f2
Lucia Acosta <luciazgz93@gmail.com>
2022-03-29 15:52:00 +0200

    animations

---
+++ src/App.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="container">
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    <transition>
+      <!-- transition solo debería tener un hijo directo -->
+      <p v-if="paraIsVisible">Solo se ve a veces este parrafo</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle paragraph</button>
+  </div>
+  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
+  <div class="container">
+    <button @click="showDialog">Show Dialog</button>
+  </div>
+</template>  
+
+<script>
+export default {
+  data() {
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false
+    };
+  },
+  methods: {
+    showDialog() {
+      this.dialogIsVisible = true;
+    },
+    hideDialog() {
+      this.dialogIsVisible = false;
+    },
+    animateBlock() {
+      this.animatedBlock = true
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform 0.3s; */
+}
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
+  /* forwards para mantener como posicion la que obtiene tras la animación, para que no vuelva a su posicion original */
+}
+/* v-enter son etiquetas que crea y entiende vue para el componente <transition> */
+.v-enter-from{
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.v-enter-active{
+  transition: all 0.3s ease-out;
+}
+.v-enter-to{
+  opacity: 1;
+  transform: translateY(0);
+}
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+</style>
